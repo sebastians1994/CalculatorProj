e2d6d2f20362c7f31daa93f634fef0115a416992
sebastians1994 <40479606+sebastians1994@users.noreply.github.com>
2020-10-07 18:22:08 -0700

    Sebastian Main

--- main.cpp
+++ main.cpp
@@ -6,5 +6,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     CalculatorProj w; 
     w.show();
-    return a.exec();   //Hi Team
+    return a.exec();   //Hello world from second branch
 }
